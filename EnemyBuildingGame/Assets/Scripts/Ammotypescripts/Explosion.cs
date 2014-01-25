2f68378f6d2e51cca2b52781685d621567fae4f0
Timo Moisio <timohmoisio@gmail.com>
2014-01-26 01:46:24 +0200

    Add explosion that does damage and pushes objects away

--- EnemyBuildingGame/Assets/Scripts/Ammotypescripts/Explosion.cs
+++ EnemyBuildingGame/Assets/Scripts/Ammotypescripts/Explosion.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Explosion : MonoBehaviour {
 
 	public float damage;
 	public float damageTime;
 	public float timestamp;
+	public GameObject visualPrefab;
+
+	private float radius;
+
+	private List<GameObject> targets;
 
 	// Use this for initialization
 	void Start () {
-	
-		transform.localScale*=5.0f;
 		timestamp = Time.time;
+		if (visualPrefab != null)
+		{
+			GameObject go = Instantiate(visualPrefab, transform.position, Quaternion.identity) as GameObject;
+			Destroy(go, 10);
+		}
+		targets = new List<GameObject>();
+		radius = GetComponent<SphereCollider>().radius;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Time.time>timestamp+damageTime) {
-	
 			Destroy (gameObject);
 		}
 
@@ -34,18 +44,33 @@ public class Explosion : MonoBehaviour {
 		else
 		{
 			target = col.gameObject;
-		}	
-		Damage dam = new Damage();
-		dam.amount = damage;
-		dam.source = this;
-		dam.targetCollider = col;
-		
-		DamageReceiver receiver = target.GetComponent<DamageReceiver>();
-		if (receiver != null)
-		{
-			//Debug.Log("bum");
-			receiver.TakeDamage(dam);
 		}
+
+		if (!targets.Contains(target))
+		{
+			targets.Add(target);
+			float dist = (target.transform.position - transform.position).sqrMagnitude;
+
+			damage *= dist / (radius * radius);
+
+			Damage dam = new Damage();
+			dam.amount = damage;
+			dam.source = this;
+			dam.targetCollider = col;
+			
+			DamageReceiver receiver = target.GetComponent<DamageReceiver>();
+			if (receiver != null)
+			{
+				Debug.Log("explo to " + receiver.gameObject.name + " " + dam.amount);
+				receiver.TakeDamage(dam);
+			}
+
+			if (target.rigidbody != null)
+			{
+				target.rigidbody.AddExplosionForce(damage * 100, transform.position, radius);
+			}
+		}
+
 		
 	}
 }
