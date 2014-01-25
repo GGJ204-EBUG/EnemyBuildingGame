c0a619d4483d6f070353781402cc803418ac93aa
Timo Moisio <timohmoisio@gmail.com>
2014-01-25 11:12:08 +0200

    Allow setting null parts

--- EnemyBuildingGame/Assets/Scripts/Slot.cs
+++ EnemyBuildingGame/Assets/Scripts/Slot.cs
@@ -3,25 +3,27 @@ using System.Collections;
 using System;
 
 public class Slot{ 
+	Vector3 position;
+	Quaternion direction;
+	private Type type;
+	private Part attachment;
+
 	public Slot(Type type, Vector3 position, Quaternion direction){
-		part = null;
 		this.position = position;
 		this.direction = direction;
 		this.type =  type;
 	}
-	private Type type;
-	Part part{
-		set{
-			if(type.IsAssignableFrom(value.GetType())){
-				part = value;
+
+	public Part Attachment {
+		set {
+			if(value == null || type.IsAssignableFrom(value.GetType())){
+				attachment = value;
 			}else{
 				throw new Exception("Mitä jäbä duunaa?");
 			}
 		}
-		get{
-			return part;
+		get {
+			return attachment;
 		}
 	}
-	Vector3 position;
-	Quaternion direction;
 }
