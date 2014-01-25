b0ca3273f1e044658b951fa03fbe40553929586b
Timo Moisio <timohmoisio@gmail.com>
2014-01-25 12:19:06 +0200

    Add EBG core

---
+++ EnemyBuildingGame/Assets/Scripts/EBG.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EBG : MonoBehaviour
+{
+	private static EBG instance;
+	public static EBG Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				GameObject go = Resources.Load("EBG") as GameObject;
+				instance = go.GetComponent<EBG>();
+			}
+			return instance;
+		}
+	}
+
+	[SerializeField]
+	private Player player1;
+	public Player Player1
+	{
+		get
+		{
+			return player1;
+		}
+	}
+
+	[SerializeField]
+	private Player player2;
+	public Player Player2
+	{
+		get
+		{
+			return player2;
+		}
+	}
+
+	#region static convenience methods
+	public static Player P1
+	{
+		get { return Instance.Player1; }
+	}
+
+	public static Player P2
+	{
+		get { return Instance.Player2; }
+	}
+	#endregion
+}
