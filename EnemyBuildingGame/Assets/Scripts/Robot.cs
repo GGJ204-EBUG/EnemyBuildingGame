3187009dae99a4a65f2747a48563b91c23b70cfd
Timo Moisio <timohmoisio@gmail.com>
2014-01-25 13:08:10 +0200

    Robots accept target heading and speed

--- EnemyBuildingGame/Assets/Scripts/Robot.cs
+++ EnemyBuildingGame/Assets/Scripts/Robot.cs
@@ -8,6 +8,33 @@ public class Robot : MonoBehaviour {
 	public float rotateSpeed = 10;
 	public float acceleration = 10;
 
+	public float TargetHeading { get; set; }
+	public float TargetAcceleration { get; set; }
+
+	public float CurrentHeading { get; private set; }
+	public float CurrentAcceleration { get; private set; }
+
+	void Update()
+	{
+		CurrentHeading = Mathf.MoveTowards(CurrentHeading, TargetHeading, rotateSpeed * Time.deltaTime);
+
+	}
+
+	void FixedUpdate()
+	{
+		CurrentAcceleration = Mathf.MoveTowards(CurrentAcceleration, TargetAcceleration, acceleration * Time.deltaTime);
+	}
+
+	public void AccelerateTowards(float targetAcceleration)
+	{
+		TargetAcceleration = targetAcceleration;
+	}
+
+	public void TurnTowards(float targetHeading)
+	{
+		this.TargetHeading = targetHeading;
+	}
+
 	public void Accelerate(float amount)
 	{		
 		rigidbody.AddRelativeForce(Vector3.forward * acceleration * amount, ForceMode.Acceleration);
