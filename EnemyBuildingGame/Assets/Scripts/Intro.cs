a3dd4de96f4235c03382c730c3071a380a399911
Timo Moisio <timohmoisio@gmail.com>
2014-01-26 09:35:55 +0200

    Splash->intro->selection->fight

---
+++ EnemyBuildingGame/Assets/Scripts/Intro.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+	public AudioClip music;
+
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		AudioManager.Instance.PlayMusic(music, true);
+	}
+
+	public void Finish()
+	{
+		anim.StopPlayback();
+
+		Invoke("Finally", 1f);
+	}
+
+	void Finally()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
