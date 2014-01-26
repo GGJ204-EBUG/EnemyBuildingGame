bfae87f87f82b5a3b1039f91b90b6a5df6afcff6
Timo Moisio <timohmoisio@gmail.com>
2014-01-26 13:59:48 +0200

    Health display

---
+++ EnemyBuildingGame/Assets/Scripts/HealthText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthText : MonoBehaviour {
+	public TextMesh text;
+	public DamageReceiver robot;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (robot != null)
+		{
+			float perc = 1 - robot.Damage / robot.health;
+			text.text = Mathf.RoundToInt(perc * 100).ToString() + "%";
+		}
+	}
+
+	void LateUpdate()
+	{
+		transform.LookAt(Camera.main.transform.position);
+	}
+}
