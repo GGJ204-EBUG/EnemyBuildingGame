8a0e1f8b008814ba10a8b26ae0acf42840f704ee
Timo Moisio <timohmoisio@gmail.com>
2014-01-26 13:04:58 +0200

    Mine thrower

--- EnemyBuildingGame/Assets/Scripts/Ammotypescripts/Mine.cs
+++ EnemyBuildingGame/Assets/Scripts/Ammotypescripts/Mine.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Mine : MonoBehaviour {
 
 	public float fuseLength;
+	public float minFuseLength = 1;
 	public float throwTime;
 	bool exploded = false;
 	public GameObject explosion;
@@ -22,28 +23,36 @@ public class Mine : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Time.time>(throwTime+fuseLength) && exploded == false) {
-
-			if (explosion != null)
-			{
-				GameObject go = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
-				if (go.audio != null) go.audio.PlayScheduled(MusicEventManager.Instance.GetNext());
-				Destroy(go, 10);
-			}
-
-			if(damageInstance != null) {
-
-				GameObject go = Instantiate(damageInstance.gameObject, transform.position, transform.rotation) as GameObject;
-				Explosion exp = go.GetComponent<Explosion>();
-				exp.damage=damage;
-
-
-			}
-
-			Destroy(gameObject);
-			exploded = true;
-			//Create explosion
-			//Destroy (gameObject);
-
+			Explode();
 		}
+	}
+
+	void Explode()
+	{
+		if (explosion != null)
+		{
+			GameObject go = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+			if (go.audio != null) go.audio.PlayScheduled(MusicEventManager.Instance.GetNext());
+			Destroy(go, 10);
+		}
+		
+		if(damageInstance != null) {
+			
+			GameObject go = Instantiate(damageInstance.gameObject, transform.position, transform.rotation) as GameObject;
+			Explosion exp = go.GetComponent<Explosion>();
+			exp.damage=damage;
+			
+			
+		}
+		
+		Destroy(gameObject);
+		exploded = true;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (Time.time < throwTime + minFuseLength) return;
+
+		Explode();
 	}
 }
